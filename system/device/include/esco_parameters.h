a095885d65505e0027680e24bbdb953dd9661ec0
goenjoy <goenjoy@namtso.com>
2023-11-23 18:16:09 +0800

    BT-mic: fix bt mic cannot work [2/2]
    Change-Id: I532794d476d5b83958503c22fab54a9e3aef6669
    Signed-off-by: goenjoy <goenjoy@namtso.com>

--- system/device/include/esco_parameters.h
+++ system/device/include/esco_parameters.h
@@ -61,7 +61,7 @@ typedef uint8_t esco_pcm_data_format_t;
 
 // SCO Data Path
 #ifndef ESCO_DATA_PATH_PCM
-#define ESCO_DATA_PATH_PCM 1                /* 0x01-0xFE (PCM Chan) */
+#define ESCO_DATA_PATH_PCM 6                /* 0x01-0xFE (PCM Chan) */
 #endif
 #define ESCO_DATA_PATH_HCI 0                /* HCI-0, 0x01-0xFE (PCM Chan) */
 typedef uint8_t esco_data_path_t;
